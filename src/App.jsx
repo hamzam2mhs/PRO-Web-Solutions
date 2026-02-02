f3188bef628331c8b72476c8c220e9d39d9272e4
hamzam2mhs <hamzam2@myumanitoba.ca>
2026-02-02 01:17:08 -0700

    fixed file path names.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import Services from './pages/Services'
 import Process from './pages/Process'
-import Faq from './pages/FAQ'
+import Faq from './pages/Faq'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
@@ -17,7 +17,7 @@ export default function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/services" element={<Services />} />
                 <Route path="/process" element={<Process />} />
-                <Route path="/FAQ" element={<FAQ />} />
+                <Route path="/faq" element={<Faq />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
